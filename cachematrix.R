c56d41628301eb76cbfb7a50b5545941a11791c2
toddbigelow <toddbigelow@gmail.com>
2015-01-23 23:16:06 -0700

    updated makeCacheMatrix and cacheSolve to handle invertible matrices and always return the inverted matrix if it exists

--- cachematrix.R
+++ cachematrix.R
@@ -3,13 +3,45 @@
 
 ## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
+#makeCacheMatrix <- function(x = matrix()) {
 
-}
+#}
 
 
 ## Write a short comment describing this function
 
-cacheSolve <- function(x, ...) {
+#cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+#}
+
+
+makeCacheMatrix <- function(x = numeric()) {
+  m <- NULL
+  print(environment())
+  evn <- environment()
+  print(parent.env(evn))
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setinverse <- function(inverse) m <<- solve(x)
+  getinverse <- function() m
+  getevn<- function() environment()
+  list(set = set, get = get,
+       setinverse = setinverse,
+       getinverse = getinverse,
+       getevn = getevn)
+}
+
+cacheSolve <- function(x, ...) {
+  m <- x$getinverse()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setinverse(m)
+  m
 }
